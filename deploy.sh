965ed905515c180a7e262514b75337e7d9fae4f7
davay <davay@defucc.me>
2023-06-27 01:00:58 +0700

    new repo

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 cd .vitepress/dist
 
 git init
-git remote add origin git@github.com:davay42/synths.git 
+git remote add origin git@github.com:chromatone/synths.git 
 git add . --force
 git commit -m 'deploy'
 
