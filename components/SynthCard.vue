4d12241683a838708ad151325fd815895b640f23
davay <davay@defucc.me>
2023-07-18 16:37:08 +0700

    client

--- components/SynthCard.vue
+++ components/SynthCard.vue
@@ -3,7 +3,7 @@ import { computed, onMounted, ref, watch } from 'vue'
 import { checkAvailability, isFormOpen, isAccessGranted } from '../composables/useForm.ts';
 import { DragHandle } from 'vue-slicksort';
 //@ts-ignore
-// import SynthFav from './SynthFav.vue';
+import SynthFav from './SynthFav.vue';
 
 const props = defineProps({
   off: { type: Boolean, default: false },
@@ -56,7 +56,8 @@ button.flex.flex-col.text-left.relative(
       .w-2.h-2.rounded-full.shadow-inset(
         :class="{'bg-green-500': online === true, 'bg-red-500':online === false}"
         )
-      //- SynthFav.scale-70.absolute.top-2.right-2(:url="url")
+      ClientOnly
+        SynthFav.scale-70.absolute.top-2.right-2(:url="url")
       DragHandle.scale-80.opacity-40.cursor-grab
         svg(xmlns="http://www.w3.org/2000/svg", width="32", height="32", viewBox="0 0 32 32")
           path(d="M4 7v2h24V7zm0 8v2h24v-2zm0 8v2h24v-2z", fill="#888888")
