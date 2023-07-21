1ca4563d2bfcdd005c36ac12df4d3d86238a3690
davay <davay@defucc.me>
2023-07-22 01:06:35 +0700

    isSent

--- composables/useForm.ts
+++ composables/useForm.ts
@@ -9,17 +9,23 @@ const directus = new Directus('https://dir.defucc.me');
 
 export const isAccessGranted: RemovableRef<boolean | string> = useStorage('access-granted', false)
 
+const isSent = ref(false)
+
 export const email = ref('')
 export const isValidEmail = computed(() => /^[^@]+@\w+(\.\w+)+\w$/.test(email.value))
 
 export async function grantAccess(email: string) {
   const data = { email }
   isAccessGranted.value = email
-  try {
-    await directus.items('players').createOne(data)
-  } catch (e) {
-    console.error(e)
+  if (!isSent.value) {
+    isSent.value = true
+    try {
+      await directus.items('players').createOne(data)
+    } catch (e) {
+      console.error(e)
+    }
   }
+
 }
 
 export const isFormOpen = ref(false)
