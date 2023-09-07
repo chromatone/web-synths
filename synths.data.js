915bc326fa2bcf8b59232e7b538009f8f8dec6e2
davay <davay@defucc.me>
2023-09-08 00:07:56 +0700

    cleaner builds

--- synths.data.js
+++ synths.data.js
@@ -4,17 +4,11 @@ import path from "node:path";
 import { fileURLToPath } from "node:url";
 import fs from 'node:fs'
 import { Directus } from '@directus/sdk';
-import { parse, stringify } from 'yaml'
-
-import { version } from './package.json'
 
 export default {
   async load() {
 
     const dirname = path.dirname(fileURLToPath(import.meta.url));
-    const cacheFile = path.resolve(dirname, `./synths-${version}.yaml`)
-
-    const cached = fs.existsSync(cacheFile) ? parse(fs.readFileSync(cacheFile, 'utf8')) : []
 
     const directus = new Directus('https://db.chromatone.center');
 
@@ -23,21 +17,19 @@ export default {
     const records = res.data
 
     for (let r of records) {
-
-      cached.push(r)
-
       let dest = path.resolve(dirname, './public/cover/')
+
+      if (!fs.existsSync(dest)) {
+        fs.mkdirSync(dest, { recursive: true });
+      }
+
       let filePath = path.resolve(dest, `${r.title.toLowerCase().split(' ').join('-')}.webp`)
       if (fs.existsSync(filePath)) continue
       let url = `https://db.chromatone.center/assets/${r.cover}?quality=70&width=1000&format=webp&download`
       console.log('downloading file:', r.title.toLowerCase().split(' ').join('-') + '.webp')
-
       await download.image({ url, dest: filePath })
     }
 
-    fs.writeFileSync(cacheFile, stringify(cached))
-
     return records
-
   }
 }
