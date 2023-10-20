7f87ec71fb5286721249c962ae5170586c2dd29e
davay <davay@defucc.me>
2023-10-20 18:09:08 +0700

    hide invite

--- composables/useForm.js
+++ composables/useForm.js
@@ -110,22 +110,22 @@ export async function grantAccess() {
         });
       console.log(await response?.json())
 
-      const invite = await fetch(
-        'https://db.chromatone.center/flows/trigger/c5ef0e6e-453d-4698-a995-e749dd331b7d',
-        {
-          method: "POST", // *GET, POST, PUT, DELETE, etc.
-          mode: "cors", // no-cors, *cors, same-origin
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, *same-origin, omit
-          headers: {
-            "Content-Type": "application/json"
-          },
-          redirect: "follow", // manual, *follow, error
-          referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-          body: JSON.stringify(data), // body data type must match "Content-Type" header
-        });
+      // const invite = await fetch(
+      //   'https://db.chromatone.center/flows/trigger/c5ef0e6e-453d-4698-a995-e749dd331b7d',
+      //   {
+      //     method: "POST", // *GET, POST, PUT, DELETE, etc.
+      //     mode: "cors", // no-cors, *cors, same-origin
+      //     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      //     credentials: "same-origin", // include, *same-origin, omit
+      //     headers: {
+      //       "Content-Type": "application/json"
+      //     },
+      //     redirect: "follow", // manual, *follow, error
+      //     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      //     body: JSON.stringify(data), // body data type must match "Content-Type" header
+      //   });
 
-      console.log(await invite?.json())
+      // console.log(await invite?.json())
 
     } catch (e) {
       console.error(e, e?.errors?.[0]?.message, e?.response?.status)
