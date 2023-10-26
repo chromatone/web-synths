ab5b3b2503be19182fef01d87bbb6abc2e6756b9
davay <davay@defucc.me>
2023-10-26 23:25:37 +0700

    a18y

--- components/MainLayout.vue
+++ components/MainLayout.vue
@@ -13,14 +13,18 @@ const { theme, site, isDark } = useData()
   .w-full.backdrop-blur-xl.sticky.top-0.z-100.bg-light-200.bg-opacity-60.dark-bg-dark-300.dark-bg-opacity-60
     .flex.flex.items-center.w-full.max-w-180.mx-auto.gap-4.px-2
       a.p-2.flex.items-center.gap-4(href="/")
-        img.w-12(:src="theme.logo")
+        img.w-12(:src="theme.logo" alt="Web-synths collection logo")
         .text-xl.font-600 {{ site.title }}
       .flex-1
-      button.text-xl(@click="isDark=!isDark")
+      button.text-xl(
+        @click="isDark=!isDark"
+        aria-label="Dark mode toggle"
+        )
         .i-la-sun(v-if="!isDark")
         .i-tabler-moon-2(v-else)
 
       button.p-4.z-2000.cursor-pointer(
+        aria-label="Player profile"
         @click="isFormOpen=!isFormOpen")
         .i-ph-user-circle-duotone.p-5
 
