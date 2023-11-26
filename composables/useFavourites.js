28880313881dab204786a317ee846022508d6ebc
davay <davay@defucc.me>
2023-11-26 16:15:15 +0700

    default favs

--- composables/useFavourites.js
+++ composables/useFavourites.js
@@ -1,4 +1,5 @@
 
 import { useStorage } from '@vueuse/core';
 
-export const favourites = useStorage('favourites', {})
+export const favourites = useStorage('favourites', { "3": true, "29": true, "39": true, })
+
